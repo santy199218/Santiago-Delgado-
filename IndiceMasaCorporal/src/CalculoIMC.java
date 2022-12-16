f0dbf5624c11e5a715c0381011ce25f9a926dd0b
santy199218 <santiagodelg@hotmail.com>
2022-12-16 17:08:03 -0500

    Actualización
    Actualización de programación

--- IndiceMasaCorporal/src/CalculoIMC.java
+++ IndiceMasaCorporal/src/CalculoIMC.java
@@ -1,29 +1,11 @@
 
 public class CalculoIMC {
-	/*
-	 * Este programa cálcula el Índice de Masa Corporal del individuo, la fórmula es
-	 * peso/estatura al cuadrado, donde el peso está en kilogramos y estatura en
-	 * metro
-	 */
-
-	/*
-	 * < 16 Delgadez severa,[25-30) Sobrepeso,[16-17) Delgadez moderada,[30-35)
-	 * Obesidad leve, [17-18.5) Delgadez leve, [35-40) Obesidad moderada, [18.5-25)
-	 * Peso normal, >=40 Obesidad mórbida
-	 */
-	static double peso;
-	static double estatura;
-
-	
-
-	public CalculoIMC() {
-		// TODO Auto-generated constructor stub
-	}
-
-
-
-	public double CalcularIMC(double imc) {
+		
+	public double CalcularIMC(double peso, double estatura) {
+		double imc;
 		imc = peso / Math.pow(estatura, 2);
 		return imc;
 	}
+
+
 }
--- IndiceMasaCorporal/src/ResultadoPeso.java
+++ IndiceMasaCorporal/src/ResultadoPeso.java
@@ -1,17 +1,15 @@
 import java.util.Scanner;
 
 public class ResultadoPeso extends CalculoIMC {
-	static double peso;
-	static double estatura;
+	
 	private static Scanner lectura;
 
-	public ResultadoPeso() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-
+	
 	public static void main(String[] args) {
-
+		double peso= 0.0;
+		double estatura=0.0;
+		double imc;
+		
 		lectura = new Scanner(System.in);
 
 		System.out.print("Hola, ingresa tu peso");
@@ -20,44 +18,45 @@ public class ResultadoPeso extends CalculoIMC {
 		System.out.print("Por favor ingresa tu estatura");
 		estatura = lectura.nextDouble();
 
-		System.out.print("El peso es de" + peso + "y la estatura de" + estatura);
-
-		double imc;
-		ResultadoPeso calculo = new ResultadoPeso();
+			
+		CalculoIMC calculo = new CalculoIMC();
 		imc = calculo.CalcularIMC(peso, estatura);
+		
+		System.out.print("El IMC es:"+imc+"\n");
 		
 
 		if (imc < 16) {
-			System.out.print("Delgadez Severa");
+			System.out.print("DELGADEZ SEVERA");
 		}
 
 		else if (imc > 16 && imc <= 17) {
-			System.out.print("Delgadez moderada");
+			System.out.print("DELGADEZ MODERADA");
 		}
 
 		else if (imc > 17 && imc <= 18.5) {
-			System.out.print("Delgadez leve");
+			System.out.print("DELGADEZ LEVE");
 		}
 
 		else if (imc > 18.5 && imc <= 25) {
-			System.out.print("Peso normal");
+			System.out.print("PESO NORMAL");
 		}
 
 		else if (imc > 25 && imc <= 30) {
-			System.out.print("Sobrepeso");
+			System.out.print("SOBREPESO");
 		}
 
 		else if (imc > 30 && imc <= 35) {
-			System.out.print("Obesidad leve");
+			System.out.print("OBESIDAD LEVE");
 		}
 
 		else if (imc > 35 && imc <= 40) {
-			System.out.print("Obesidad moderada");
+			System.out.print("OBESIDAD MODERADA");
 		}
 
 		else {
-			System.out.print("Obesidad mórbida");
+			System.out.print("OBESIDAD MORBIDA");
 		}
 	}
 
-}
+		}
+
