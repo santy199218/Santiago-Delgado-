d5f603c06339ad95e7e865cc66dff38aaf6c3e0a
santy199218 <santiagodelg@hotmail.com>
2022-12-16 16:38:37 -0500

    Actualización
    Actualización en programa

--- IndiceMasaCorporal/src/ResultadoPeso.java
+++ IndiceMasaCorporal/src/ResultadoPeso.java
@@ -3,14 +3,12 @@ import java.util.Scanner;
 public class ResultadoPeso extends CalculoIMC {
 	static double peso;
 	static double estatura;
+	private static Scanner lectura;
 
 	public ResultadoPeso() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-
-	private static final double imc = 0;
-	private static Scanner lectura;
 
 	public static void main(String[] args) {
 
@@ -24,50 +22,42 @@ public class ResultadoPeso extends CalculoIMC {
 
 		System.out.print("El peso es de" + peso + "y la estatura de" + estatura);
 
+		double imc;
 		ResultadoPeso calculo = new ResultadoPeso();
-		calculo.CalcularIMC(imc);
-		peso = imc;
+		imc = calculo.CalcularIMC(peso, estatura);
+		
 
 		if (imc < 16) {
 			System.out.print("Delgadez Severa");
-		} else {
-			if (imc > 16 && imc <= 17) {
-				System.out.print("Delgadez moderada");
-			}
+		}
 
-			else {
-				if (imc > 17 && imc <= 18.5) {
-					System.out.print("Delgadez leve");
-				}
+		else if (imc > 16 && imc <= 17) {
+			System.out.print("Delgadez moderada");
+		}
 
-				else {
-					if (imc > 18.5 && imc <= 25) {
-						System.out.print("Peso normal");
-					}
+		else if (imc > 17 && imc <= 18.5) {
+			System.out.print("Delgadez leve");
+		}
 
-					else {
-						if (imc > 25 && imc <= 30) {
-							System.out.print("Sobrepeso");
-						}
+		else if (imc > 18.5 && imc <= 25) {
+			System.out.print("Peso normal");
+		}
 
-						else {
-							if (imc > 30 && imc <= 35) {
-								System.out.print("Obesidad leve");
-							}
+		else if (imc > 25 && imc <= 30) {
+			System.out.print("Sobrepeso");
+		}
 
-							else {
-								if (imc > 35 && imc <= 40) {
-									System.out.print("Obesidad moderada");
-								}
+		else if (imc > 30 && imc <= 35) {
+			System.out.print("Obesidad leve");
+		}
 
-								else {
-									System.out.print("Obesidad mórbida");
-								}
-							}
-						}
-					}
-				}
-			}
+		else if (imc > 35 && imc <= 40) {
+			System.out.print("Obesidad moderada");
+		}
+
+		else {
+			System.out.print("Obesidad mórbida");
 		}
 	}
+
 }
